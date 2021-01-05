1809606386470dc59106ea9c195d5480c65dd8c0
gedemais <gedemais.dev@gmail.com>
2021-01-05 22:34:51 +0100

    setup of the project

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # ft_linear_regression
 First 42's machine learning branch project
 
-<h1> Setup </h1>
 
-- [x] faire ta daronne
+<h2>Todo :</h2>
+
+
+<h2>Blocked :</h2>
+
+<h2>In Progress :</h2>
+
+<h2>Testing :</h2>
+
+<h2>Done :</h2>
+
+<h2>Backlog :</h2>
+
+<h2> ---------- Done Backlog ---------- </h2>
+
+<h2> Ideas </h2>
+Faire passer theta0 et theta1 en variable d'environnement entre l'execution
+du trainer et du solver
---
+++ the_first_program.py
@@ -0,0 +1,35 @@
+from sys import stderr, stdout
+import os
+
+keep = True
+
+while keep:
+    keep = False
+    try:
+        s = input("Enter a mileage (or q to quit)-> ")
+
+        if s == "q":
+            print("Goodbye !")
+            break
+
+        mileage = int(s)
+
+    except ValueError:
+
+        print("This is not a correct mileage !", file=stderr)
+        keep = True
+
+theta_0 = os.getenv("THETA_ZERO")
+theta_1 = os.getenv("THETA_ONE")
+
+which = "THETA_ZERO" if not theta_0 else "THETA_ONE"
+
+if not theta_0 or not theta_1:
+    print(which + " environnement variable not found !", file=stderr)
+    exit(1)
+
+try:
+    t0 = float(theta_0)
+    t1 = float(theta_1)
+except:
+    print("THETA_ZERO/THETA_ONE environnement variables content is not valid !", file=stderr)
